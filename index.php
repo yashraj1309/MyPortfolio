0c437cbf0af944ce11b83203428eb17344d7de8a
Yashraj Dudhatra <77092074+yashraj1309@users.noreply.github.com>
2021-11-20 05:18:14 -1000

    portfolio

---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/Portfolio.css">
+        <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+        <title>Digital Portfolio</title>
+    </head>
+    <body>
+
+         <!-- HEADER -->
+         <header class="l-header">
+            <nav class="nav bd-grid">
+                <div>
+                    <a href="#" class="nav__logo">Yashraj Dudhatra</a>
+                </div>
+
+                <div class="nav__menu" id="nav-menu">
+                    <ul class="nav__list">
+                        <li class="nav__item"><a href="#home" class="nav__link active">Home</a></li>
+                        <li class="nav__item"><a href="#about" class="nav__link">About</a></li>
+                        <li class="nav__item"><a href="#skills" class="nav__link">Skills</a></li>
+                        <li class="nav__item"><a href="#work" class="nav__link">Certificates</a></li>
+                        <li class="nav__item"><a href="#contact" class="nav__link">Contact</a></li>
+                    </ul>
+                </div>
+
+                <div class="nav__toggle" id="nav-toggle">
+                    <i class='bx bx-menu'></i>
+                </div>
+            </nav>
+        </header>
+
+        <main class="l-main">
+            <!--===== HOME =====-->
+            <section class="home bd-grid" id="home">
+                <div class="home__data">
+                    <h1 class="home__title">Hi,<br>I'am <span class="home__title-color">Yashraj</span><br>Web Developer</h1>
+
+                    <a href="#contact" class="button">Contact</a>
+                </div>
+
+                <div class="home__social">
+                    <a href="https://in.linkedin.com/in/yash-patel-3033621b8" class="home__social-icon" target="_blank"><i class='bx bxl-linkedin'></i></a>
+                    <a href="https://github.com/yashraj1309" class="home__social-icon"><i class='bx bxl-github' target="_blank" ></i></a>
+                    <a href="https://www.blogger.com/profile/13955354845569995091" class="home__social-icon" target="_blank"><i class='bx bxl-blogger' ></i></a>
+                    <a href="https://www.instagram.com/yashraj_dudhatra/" class="home__social-icon" target="_blank"><i class='bx bxl-instagram' ></i></a>
+                </div>
+
+                <div class="home__img">    
+                    <img src="profile.png" alt="">
+                </div>
+            </section>
+
+            <!--===== ABOUT =====-->
+            <section class="about section " id="about">
+                <h2 class="section-title">About</h2>
+
+                <div class="about__container bd-grid">
+                    <div class="about__img">
+                        <img src="about.png" alt="">
+                    </div>
+                    
+                    <div>
+                        <h2 class="about__subtitle">I'am Yashraj</h2>
+                        <p class="about__text">I design and code beautifully simple things, and I love what I do. Currently i am a college student in LD college of Engineering Persuing my bechlors degree in IT. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.
+                        </p>           
+                    </div>                                   
+                </div>
+            </section>
+
+            <!--===== SKILLS =====-->
+            <section class="skills section" id="skills">
+                <h2 class="section-title">Skills</h2>
+
+                <div class="skills__container bd-grid">          
+                    <div>
+                        <h2 class="skills__subtitle">Profesional Skills</h2>
+                        <p class="skills__text">I have gain knowledge from my college and from various platfroms. I'm sure that i will give my best work with this skill set.</p>
+                        <div class="skills__data">
+                            <div class="skills__names">
+                                <i class='bx bxl-html5 skills__icon'></i>
+                                <span class="skills__name">HTML5</span>
+                            </div>
+                            <div class="skills__bar skills__html">
+
+                            </div>
+                            <div>
+                                <span class="skills__percentage">90%</span>
+                            </div>
+                        </div>
+                        <div class="skills__data">
+                            <div class="skills__names">
+                                <i class='bx bxl-css3 skills__icon'></i>
+                                <span class="skills__name">CSS3</span>
+                            </div>
+                            <div class="skills__bar skills__css">
+                                
+                            </div>
+                            <div>
+                                <span class="skills__percentage">80%</span>
+                            </div>
+                        </div>
+                        <div class="skills__data">
+                            <div class="skills__names">
+                                <i class='bx bxl-javascript skills__icon' ></i>
+                                <span class="skills__name">JAVASCRIPT</span>
+                            </div>
+                            <div class="skills__bar skills__js">
+                                
+                            </div>
+                            <div>
+                                <span class="skills__percentage">60%</span>
+                            </div>
+                        </div>
+                        <div class="skills__data">
+                            <div class="skills__names">
+                                <i class='bx bxs-paint skills__icon'></i>
+                                <span class="skills__name">PHP</span>
+                            </div>
+                            <div class="skills__bar skills__ux">
+                                
+                            </div>
+                            <div>
+                                <span class="skills__percentage">75%</span>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div>              
+                        <img src="skills.png" alt="" class="skills__img">
+                    </div>
+                </div>
+            </section>
+
+            <!--===== WORK =====-->
+            <section class="work section" id="work">
+                <h2 class="section-title">Certificates</h2>
+
+                <div class="work__container bd-grid">
+                    <div class="work__img">
+                        <img src="html and html5 freecodecamp.pdf and 7 more pages - Personal - Microsoft​ Edge 19-11-2021 16_21_55 (2).png" alt="">
+                    </div>
+                    <div class="work__img">
+                        <img src="Javascript udamy basic.pdf and 8 more pages - Personal - Microsoft​ Edge 19-11-2021 16_44_43 (2).png" alt="">
+                    </div>
+                    <div class="work__img">
+                        <img src="Javascript udamy basic.pdf and 8 more pages - Personal - Microsoft​ Edge 19-11-2021 16_45_04 (2).png" alt="">
+                    </div>
+                    <div class="work__img">
+                        <img src="Javascript2.pdf and 9 more pages - Personal - Microsoft​ Edge 19-11-2021 16_55_28 (2).png" alt="">
+                    </div>
+                    <div class="work__img">
+                        <img src="html form.pdf and 10 more pages - Personal - Microsoft​ Edge 19-11-2021 17_01_31.png" alt="">
+                    </div>
+                    <div class="work__img">
+                        <img src="html form.pdf and 10 more pages - Personal - Microsoft​ Edge 19-11-2021 17_06_17.png" alt="">
+                    </div>
+                </div>
+            </section>
+            <!--===== CONTACT =====-->
+            <section class="contact section" id="contact">
+                <h2 class="section-title">Contact Me</h2>
+                <div class="contact__container bd-grid">
+                    <form action="index.php" class="contact__form" method="post" name="form" onsubmit="validate()"> 
+                        <input type="text" id="name" name="name" placeholder="Name" class="contact__input">
+                        <input type="email" id="email" name="email"placeholder="Email" class="contact__input">
+                        <textarea name="text" id="text" cols="0" rows="10" class="contact__input" placeholder="Description"></textarea>
+                        <button class="contact__button button" id="btnc">Submit</button>
+                    </form>
+                </div>
+            </section>
+        </main>
+
+        <!--===== FOOTER =====-->
+        <footer class="footer">
+            <p class="footer__title">Yashraj Dudhatra</p>
+            <div class="footer__social">
+                <a href="https://github.com/yashraj1309" class="footer__icon"><i class='bx bxl-github' ></i></a>
+                <a href="https://www.instagram.com/yashraj_dudhatra/" class="footer__icon"><i class='bx bxl-instagram' ></i></a>
+                <a href="https://www.blogger.com/profile/13955354845569995091" class="footer__icon"><i class='bx bxl-blogger' ></i></a>
+            </div>
+            <p>&#169; Made with &#10084;</p>
+        </footer>
+    </body>
+</html>
+<?php
+    if(isset($_POST['name'])){
+    $server = "localhost";
+    $username = "id17982062_yashraj1309";
+    $password = "Yashraj@2019";
+
+    $con = mysqli_connect($server, $username, $password);
+    if(!$con){
+        die("connection to this database failed due to" . mysqli_connect_error());
+    }
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $text = $_POST['text'];
+    $sql = "INSERT INTO `id17982062_form`.`form_data`(`name`, `email`, `text`) VALUES ('$name','$email','$text');";
+    if($con->query($sql) == true){
+        // echo "Successfully inserted";
+
+        // Flag for successful insertion
+        $insert = true;
+    }
+    else{
+        echo "ERROR: <br> $con->error";
+    }
+    $con->close();
+    }
+?>
+<script>
+function validate(){  
+ alert("Success");
+}
+</script> 
